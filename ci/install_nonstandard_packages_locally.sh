6e5b62f34c94fdb603dfb46622b439710f7cf2af
Stephan Hagemann <hey@stephanhagemann.com>
2023-06-28 12:05:04 -0600

    Use pip3 instead of pip

--- ci/install_nonstandard_packages_locally.sh
+++ ci/install_nonstandard_packages_locally.sh
@@ -1,6 +1,6 @@
 target="$1"
 
-pip install --target="$target" google-api-python-client google-auth-httplib2 google-auth-oauthlib requests
+pip3 install --target="$target" google-api-python-client google-auth-httplib2 google-auth-oauthlib requests
 
 # For some reason, the "google" library doesn't have an __init__.py which prevents it from being imported.
 touch "$target/google/__init__.py"
